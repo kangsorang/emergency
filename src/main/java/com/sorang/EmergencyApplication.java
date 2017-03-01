361f90625d3587eb58dda0fb404b8c993667176a
kangsorang <kangrangx@gmail.com>
2017-03-01 13:09:19 +0900

    Initial Commit

---
+++ src/main/java/com/sorang/EmergencyApplication.java
@@ -0,0 +1,12 @@
+package com.sorang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyApplication.class, args);
+	}
+}
