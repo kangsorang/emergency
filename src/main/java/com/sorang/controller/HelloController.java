7c098810d3ba368f3ada9d756220d0d9632b8814
kangsorang <kangrangx@gmail.com>
2017-03-01 13:15:27 +0900

    Test commit

--- src/main/java/com/sorang/controller/HelloController.java
+++ src/main/java/com/sorang/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@RequestMapping("/")
 	public String index() {
-		return "helloworld!";
+		return "helloworld! Nice to meet U~~";
 	}
 }
